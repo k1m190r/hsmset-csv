c93b9b66c145e732555713f67e94ef77e19c09c1
biosckon <biosckon@hotmail.com>
2016-11-29 20:36:58 +0100

    first commmit, not working

---
+++ README.md
@@ -0,0 +1,14 @@
+### Convenience utility
+Loads CSV in the specific form into redis using redis protocol.
+- using some code from https://github.com/tlehman/redis-mass
+```
+col1, col2, col3
+key,  field, value
+
+e.g.
+
+users,   ,
+     , 1 , Alice
+     , 2 , Bob
+```
+
---
+++ loadCSV.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	//	red "github.com/biosckon/redis_protocol"
+	"io"
+	"log"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func procRec(rec string) {
+	// build up the redis command and feed it to the red.
+}
+
+func main() {
+	flag.Parse()
+	files := flag.Args()
+
+	for x, fname := range files {
+		fmt.Printf("%v, %v\n", x, fname)
+
+		f, err := os.Open(fname)
+		check(err)
+
+		r := csv.NewReader(bufio.NewReader(f))
+
+		for {
+			record, err := r.Read()
+
+			switch err {
+			case io.EOF:
+				return
+			case nil:
+				break
+			default:
+				log.Fatal(err)
+			}
+
+			procRec(record)
+		}
+	}
+
+}
