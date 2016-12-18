98437840783dfb75eb35b76d4a94dbd09500cf8b
biosckon <biosckon@hotmail.com>
2016-12-18 17:31:20 +0100

    Working minimum!

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ### Convenience utility
 Loads CSV in the specific form into redis hash using redis protocol.
 Inspiration https://github.com/tlehman/redis-mass
+
+### spec
+MIME "text/csv" https://tools.ietf.org/html/rfc4180
+  
 ```
 col1, col2
 field, value
@@ -16,5 +20,13 @@ File: users.csv
 is equivalent to:
 ```
 HMSET users 1 Alice 2 Bob
+```
+If there is a comma ',' in the file name it is replaced with colon ':'. So:
+```
+users,1.csv
+```
+Will end up in the key:
+```
+users:1
 ```
 
--- hmset-csv.go
+++ hmset-csv.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/csv"
 	"flag"
+	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
+
+var buf bytes.Buffer
+var cs int
+var cf *os.File
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal()
+	}
+}
 
 func main() {
 
@@ -13,9 +30,76 @@ func main() {
 
 	for _, fname := range files {
 		f, err := os.Open(fname)
+		checkErr(err)
+
+		cf = f
+		cs = 0
+		buf.Reset()
+
 		procFile(f)
+		f.Close()
 	}
 }
 
-func procFile() {
+func procFile(f *os.File) {
+	r := csv.NewReader(bufio.NewReader(f))
+
+	for {
+		rec, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal()
+		}
+
+		addRec(rec)
+	}
+	send2redis()
+}
+
+func redStr(s string) string {
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
+}
+
+// build redis proto string and send to write buffer
+func addRec(rec []string) {
+	if len(rec) != 2 {
+		return
+	}
+
+	for i, s := range rec {
+		rec[i] = strings.TrimSpace(s)
+	}
+
+	if rec[0] == "" || rec[1] == "" {
+		return
+	}
+
+	for _, s := range rec {
+		buf.WriteString(redStr(s))
+		cs += 1
+	}
+
+}
+
+func hmset(s string) string {
+	var ret bytes.Buffer
+
+	ret.WriteString(fmt.Sprintf("*%d\r\n", cs+2)) // +2 for HMSET KEY
+	ret.WriteString(redStr("HMSET"))
+	fn := strings.Replace(filepath.Base(cf.Name()), ",", ":", -1)
+	name := strings.TrimSuffix(fn, filepath.Ext(fn))
+	ret.WriteString(redStr(name))
+	ret.WriteString(s)
+
+	rs := ret.String()
+
+	return rs
+}
+
+// count the strings, add hmset key and send to stdout
+func send2redis() {
+	vals := buf.String()
+	fmt.Printf(hmset(vals))
 }
